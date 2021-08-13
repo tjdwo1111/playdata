b9ee6015658c4f25ba7d906be93899ef57c233f4
SJY0917032 <sea71511@gmail.com>
2021-08-13 12:55:34 +0900

    gameUser - gameRoom 클래스 부분은 주석처리 했습니다.

---
+++ silhouetteGame/src/silhouetteServer/gameUser.java
@@ -0,0 +1,52 @@
+package silhouetteServer;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class gameUser extends Thread {
+
+
+  gameSever server;
+  Socket socket; // Client Socket
+  Vector<gameUser> auser;
+
+  /**
+   * gameRoom class가 추가돼야 작동됩니다. Vector<gameRoom> room;
+   */
+
+  OutputStream os;
+  DataOutputStream dos;
+  InputStream is;
+  DataInputStream dis;
+
+  String msg; // 수신할 메세지
+  String name; // gameUser's name
+
+  /**
+   * gameRoom class가 추가돼야 작동합니다. gameRoom nowRoom; // 입장한 방의 Object를 저장
+   */
+
+
+  // 메세지 구분 태그
+  // final String
+
+
+  public gameUser(Socket soc, Server sev) {
+    this.socket = soc;
+    this.server = sev;
+
+    auser = server.alluser;
+  }
+
+
+  @Override
+  public void run() {
+    System.out.println("서버에 입장합니다!" + this.socket.toString());
+
+    os = this.socket.getOutputStream();
+    dos = new DataOutputStream(os);
+    is = this.socket.getInputStream();
+    dis = new DataInputStream(is);
+  }
+}
