7e8f29bee87329fc2dd62d4ffc75c2ed3c7bebe0
jahokoo <81978506+jahokoo@users.noreply.github.com>
2021-08-13 12:55:31 +0900

    gameRoom

---
+++ silhouetteGame/src/silhouetteServer/gameRoom.java
@@ -0,0 +1,16 @@
+package silhouetteServer;
+
+import java.util.Vector;
+
+public class gameRoom {
+	Vector <gameUser> gUser;
+	String title;
+	int count = 0;
+	
+	gameRoom() {
+	 gUser = new Vector<>();
+
+	}
+	
+
+}
